769df989d0d46864c36ae1cd6bf2d80b804cdf67
scorpion <oracekpakpo0@gmail.com>
2022-02-10 12:09:16 -0500

    kretprobe bug fixed

--- tcp_kpi_tracker.c
+++ tcp_kpi_tracker.c
@@ -17,8 +17,6 @@ struct tcp_kpi_tracker_bpf *skel;
 #define FILTER_ADDR_v4(a) skel->bss->filter.addr_v4 = a;
 #define FILTER_ADDR_v6(a) memcpy(skel->bss->filter.addr_v6, a, 16);
 
-static struct kpi_events hash_table[HASH_SIZE];
-
 static struct env
 {
   bool verbose;
@@ -56,13 +54,13 @@ const char argp_program_doc[] =
 static const struct argp_option opts[] = {
     {"verbose", 'v', NULL, 0, "Verbose debug output"},
     {"duration", 'd', "DURATION-MS", 0, "Minimum process duration (ms) to report"},
-    {"bictcp_init", 'i', NULL, 0, "Enable tracing for cubic's init function"},
-    {"bictcp_cwnd_event", 'c', NULL, 0, "Enable tracing for cubic's cwnd_event function"},
-    {"bictcp_recal_ssthresh", 'r', NULL, 0, "Enable tracing for cubic's recal_ssthresh function"},
-    {"bictcp_state", 's', NULL, 0, "Enable tracing for cubic's state function"},
-    {"bictcp_acked", 'a', NULL, 0, "Enable tracing for cubic's acked function"},
-    {"bictcp_cong_avoid", 'o', NULL, 0, "Enable tracing for cubic's cong_avoid function"},
-    {"bictcp_cong_algorithms", 'n', NULL, 0, "Tracing all cubic's functions"},
+    {"cubictcp_init", 'i', NULL, 0, "Enable tracing for cubic's init function"},
+    {"cubictcp_cwnd_event", 'c', NULL, 0, "Enable tracing for cubic's cwnd_event function"},
+    {"cubictcp_recal_ssthresh", 'r', NULL, 0, "Enable tracing for cubic's recal_ssthresh function"},
+    {"cubictcp_state", 's', NULL, 0, "Enable tracing for cubic's state function"},
+    {"cubictcp_acked", 'a', NULL, 0, "Enable tracing for cubic's acked function"},
+    {"cubictcp_cong_avoid", 'o', NULL, 0, "Enable tracing for cubic's cong_avoid function"},
+    {"cubictcp_cong_algorithms", 'n', NULL, 0, "Tracing all cubic's functions"},
     {"ip", 'p', "AF", 0, "IP protocol, --ip=4 for ipv4 and --ip=6 for ipv6"},
     {"ip4addr", 'x', "IP4-ADDR", 0, "IPv4 address"},
     {"ip6addr", 'y', "IP6-ADDR", 0, "IPv6 address"},
@@ -70,33 +68,6 @@ static const struct argp_option opts[] = {
     {"log_file", 'f', "LOG-FILE", 0, "Log file path"},
     {},
 };
-
-/* treat strings as base-256 integers */
-/* with digits in the range 1 to 255 */
-static unsigned long hash(const char *s, int size, unsigned long m)
-{
-  unsigned long h;
-  unsigned const char *us;
-  us = (unsigned const char *)s;
-  h = 0;
-  while (*us != '\0')
-  {
-    h = (h * BASE + *us) % m;
-    us++;
-  }
-  return h;
-}
-
-static void display_hash_table(void)
-{
-  int i;
-  for (i = 0; i < HASH_SIZE; i++)
-  {
-    if (hash_table[i].not_free)
-      printf("%ld:%d:%s:%s\n", hash_table[i].hash, hash_table[i].retrans,
-             hash_table[i].src, hash_table[i].dst);
-  }
-}
 
 /** Tips: Make sure to call this function after open skel bpf structure,
  *  otherwise a SIGSEGV signal is triggered.
@@ -119,47 +90,6 @@ static void parse_filter_args()
   skel->bss->filter.cwnd_event = (env.bictcp_cwnd_event == true ? 1 : 0);
   skel->bss->filter.cong_avoid = (env.bictcp_cong_avoid == true ? 1 : 0);
   skel->bss->filter.allsyms = (env.bictcp_cong_algorithms == true ? 1 : 0);
-}
-
-static void common_handle_event(const struct event *e, unsigned long *hashVal)
-{
-  char src[INET6_ADDRSTRLEN];
-  char dst[INET6_ADDRSTRLEN];
-  char eventstr[74];
-  int ret;
-  union
-  {
-    struct in_addr x4;
-    struct in6_addr x6;
-  } s, d;
-
-  if (e->af == AF_INET)
-  {
-    s.x4.s_addr = e->saddr_v4;
-    d.x4.s_addr = e->daddr_v4;
-  }
-  else
-  {
-    memcpy(&s.x6.s6_addr, e->saddr_v6, sizeof(s.x6.s6_addr));
-    memcpy(&d.x6.s6_addr, e->daddr_v6, sizeof(d.x6.s6_addr));
-  }
-  ret = sprintf(eventstr, "%d  %s %s %d %d %d %d %d",
-                e->af == AF_INET ? 4 : 6, inet_ntop(e->af, &d, dst, sizeof(dst)),
-                inet_ntop(e->af, &s, src, sizeof(src)), e->dport,
-                e->sport, e->state, e->portpair >> 16, e->portpair & 0xffff);
-  eventstr[ret] = '\0';
-  *hashVal = hash(eventstr, strlen(eventstr), HASH_SIZE);
-  printf("%s\n", eventstr);
-  if (hash_table[*hashVal].not_free && (strcmp(src, hash_table[*hashVal].src) ||
-                                        strcmp(dst, hash_table[*hashVal].dst)))
-    fprintf(stderr, "Collision %ld\n", *hashVal);
-  if (!hash_table[*hashVal].hash)
-  {
-    hash_table[*hashVal].hash = *hashVal;
-    strcpy(hash_table[*hashVal].src, src);
-    strcpy(hash_table[*hashVal].dst, dst);
-    hash_table[*hashVal].not_free = 1;
-  }
 }
 
 static error_t parse_arg(int key, char *arg, struct argp_state *state)
@@ -269,29 +199,15 @@ static volatile bool exiting = false;
 static void sig_handler(int sig)
 {
   exiting = true;
-  display_hash_table();
 }
 
 static int handle_event(void *ctx, void *data, size_t data_sz)
 {
   const struct event *e = (struct event *)data;
-  struct tm *tm;
-  char ts[32];
-  time_t t;
-  time(&t);
-  tm = localtime(&t);
-  unsigned long hashVal;
-  strftime(ts, sizeof(ts), "%H:%M:%S", tm);
   switch (e->type)
-  {
-    case RETRANSMIT_SKB:
-      fprintf(stderr, "RETRANSMIT_SKB\n");
-      common_handle_event(e, &hashVal);
-      hash_table[hashVal].retrans++;
-      break;    
+  {  
     case BICTCP_CONG_AVOID:
     fprintf(stderr, "BICTCP_CONG_AVOID\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
@@ -300,7 +216,6 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     break;
     case BICTCP_ACKED:
     fprintf(stderr, "BICTCP_ACKED\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
@@ -309,7 +224,6 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     break;
     case BICTCP_CWND_EVENT:
     fprintf(stderr, "BICTCP_CWND_EVENT\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
@@ -318,7 +232,6 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     break;
     case BICTCP_INIT:
     fprintf(stderr, "BICTCP_INIT\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
@@ -327,7 +240,6 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     break;
     case BICTCP_SSTHRESH:
     fprintf(stderr, "BICTCP_SSTHRESH\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
@@ -336,7 +248,6 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     break;
     case BICTCP_STATE:
     fprintf(stderr, "BICTCP_STATE\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
@@ -345,42 +256,13 @@ static int handle_event(void *ctx, void *data, size_t data_sz)
     break;
     case BICTCP_UNDO_CWND:
     fprintf(stderr, "BICTCP_UNDO_CWND\n");
-    common_handle_event(e, &hashVal);
     fprintf(stdout,"%s:%d %s:%d %s:%d\n",
     "tcp_cwnd",e->bictcp.tcp_cwnd,"last_cwnd",e->bictcp.last_cwnd,"last_max_cwnd",e->bictcp.last_max_cwnd);
     if(env.log_file){
       fprintf(env.log_file,"%d %d %d\n",e->bictcp.tcp_cwnd,e->bictcp.last_cwnd,e->bictcp.last_max_cwnd);
     }
     break;
-    case TRANSMIT_SKB:
-      fprintf(stderr, "TRANSMIT_SKB\n");
-      common_handle_event(e, &hashVal);
-      break;
-    /*case RECV_RESET:
-      fprintf(stderr, "RECV_RESET\n");
-      common_handle_event(e, &hashVal);
-      break;
-    case SEND_RESET:
-      fprintf(stderr, "SEND_RESET\n");
-      common_handle_event(e, &hashVal);
-      break;
-    case DESTROY_SOCK:
-      fprintf(stderr, "DESTROY_SOCK\n");
-      common_handle_event(e, &hashVal);
-      break;
-    case RETRANSMIT_SYNACK:
-      fprintf(stderr, "RETRANSMIT_SYNACK\n");
-      common_handle_event(e, &hashVal);
-      break;
-    case TCP_PROBE:
-      fprintf(stderr, "TCP_PROBE\n");
-      common_handle_event(e, &hashVal);
-      break;
-    case SOCK_SET_STATE:
-      fprintf(stderr, "SOCK_SET_STATE\n");
-      common_handle_event(e, &hashVal);
-      break;*/
-  default:
+      default:
     break;
     //fprintf(stderr, "UNKNOWN EVENT (%d)\n", e->type);
   }
@@ -436,10 +318,7 @@ int main(int argc, char **argv)
     fprintf(stderr, "Failed to create ring buffer\n");
     goto cleanup;
   }
-  memset(hash_table, 0, sizeof(struct kpi_events) * HASH_SIZE);
-  /* Process events */
-  printf("%-2s %-16s %-16s %-4s %-10s %-10s %-3s %-3s\n",
-         "AF", "DADDR", "SADDR", "PROTOCOL", "DPORT", "SPORT", "OLDSTATE", "NEWSTATE");
+
   while (!exiting)
   {
     err = ring_buffer__poll(rb, 100 /* timeout, ms */);
